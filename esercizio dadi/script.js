92f25272f0b8f0486dadd9d2d3b97eb26af5c830
LaSe28 <mattia.las28@gmail.com>
2022-02-24 17:07:19 +0100

    dadi

---
+++ esercizio dadi/script.js
@@ -0,0 +1,19 @@
+let btnLaunch = document.querySelector("#launch")
+let result = document.querySelector("#result")
+let text = document.querySelector("#text")
+
+btnLaunch.addEventListener("click", function(){
+    
+    let pcLaunch = Math.floor(Math.random() * (7 - 1) + 1);
+    let playerLaunch = Math.floor(Math.random() * (7 - 1) + 1);
+    result.innerHTML="Dado del PC <br>" + ' ' + pcLaunch + ' <br>' + "Dado dell'utente" + ' <br>' + playerLaunch
+    
+    if(pcLaunch > playerLaunch){   
+        text.innerHTML="Ah ah! Ho vinto io!";
+    } else if (pcLaunch < playerLaunch){
+        text.innerHTML="E' solo fortuna..";
+    } else{
+        text.innerHTML="Pareggio!";
+    }
+
+})
