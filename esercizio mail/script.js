3b0d02696463f95983585daa062e19453e4c643d
LaSe28 <mattia.las28@gmail.com>
2022-02-24 16:33:05 +0100

    mail

---
+++ esercizio mail/script.js
@@ -0,0 +1,29 @@
+let inputMail = document.querySelector(`#mailAddress`)
+let btnStart = document.querySelector(`#start`)
+let mailList = [`a@gmail.com`,`b@gmail.com`,`c@gmail.com`,`d@gmail.com`,`e@gmail.com`,`f@gmail.com`,`g@gmail.com`,`h@gmail.com`]
+let result = document.querySelector(`#result`)
+
+let authorized = false
+
+btnStart.addEventListener('click', function(){
+    
+    for (let i = 0; i < mailList.length; i++){
+        if (mailList[i] == inputMail.value ){
+            authorized = true
+        }
+    }
+
+    if (authorized == false){
+        result.innerHTML = "Non sei autorizzato!"
+    } else {
+        result.innerHTML = "Bene, sei autorizzato!";
+    }
+
+}
+)
+
+
+
+
+
+
